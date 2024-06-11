3d44637df690c09681593df9d63cfe295df9dce1
Eman Hashim <emanhashim42@gmail.com>
2024-06-10 18:45:58 -0700

    OCA class 1

---
+++ src/oracleClass1.java
@@ -0,0 +1,20 @@
+public class oracleClass1 {
+
+    String java;
+    String name;
+
+    void sound(){
+        System.out.println("this method doesnt return anything for now");
+    }
+
+    public String getJava() {
+        return java;
+    }
+
+
+    public void setJava(String newName){
+        name = newName;
+    }
+
+
+}
---
+++ src/oracleMain.java
@@ -0,0 +1,11 @@
+/**
+ * @author  Eman
+ */
+
+public class oracleMain {
+
+    public static void main(String args[]){
+        System.out.println("main");
+
+    }
+}
